f3be03d1290cc0d226b93dd68ad9e0cbce053fd5
GuiMV <guilherme.moizinho17@gmail.com>
2024-04-23 12:47:31 -0300

    Recursividade para Tablets

--- Space/script-space.js
+++ Space/script-space.js
@@ -1,5 +1,17 @@
+let scale = 1; //Zoom
+let isIntervalRunning = true;
+const mercurio = document.querySelector('.Mercurio');
+    let aMercurio = 0; // Angulo
+    const roMercurio = 92; // Raio da órbita
+    let intervalIDMercurio = setInterval(updateMercurioPosition, 8.8); //Atualização dos planetas
+const venus = document.querySelector('.Venus');
+    let aVenus = 0;
+    const roVenus = 142;
+    let intervalIDVenus = setInterval(updateVenusPosition, 24.3); //Atualização dos planetas
+
+
+
 // JavaScript para controlar o zoom com o scroll do mouse
-let scale = 1;
 document.addEventListener("wheel", function(event) {
     const zoomStep = 0.1;
     if (event.deltaY < 0) {
@@ -10,51 +22,82 @@ document.addEventListener("wheel", function(event) {
         scale -= zoomStep;
     }
 
-    scale = Math.max(0.1, Math.min(5, scale));
+    scale = Math.max(0.7, Math.min(4, scale));
     document.querySelector("main").style.transform = `scale(${scale})`;
 });
   
-// // Event listener para detectar a tecla ESPAÇO
-// document.addEventListener('keydown', function(event) {
-//     if (event.code === 'Space') {
-//         var animacaoAtiva = document.body.dataset.animacaoAtiva === 'true';
-//         if (animacaoAtiva) {
-//         desativarAnimacao();
-//         document.body.dataset.animacaoAtiva = 'false';
-//         } else {
-//         ativarAnimacao();
-//         document.body.dataset.animacaoAtiva = 'true';
-//         }
-//     }
-// });
+// Função para detectar a tecla ESPAÇO
+document.addEventListener('keydown', function(event) {
+    if (event.code === 'Space') {
+        if (isIntervalRunning) {
+            clearInterval(intervalIDMercurio); // Pausa o intervalo de Mercúrio
+            intervalIDMercurio = setInterval(updateMercurioPosition, 999);
+            clearInterval(intervalIDVenus); // Pausa o intervalo de Vênus
+            intervalIDVenus = setInterval(updateVenusPosition, 999);
+            isIntervalRunning = false; // Atualiza a variável para indicar que o intervalo não está em execução
+        } else {
+            intervalIDMercurio = setInterval(updateMercurioPosition, 8.8); // Retoma o intervalo de Mercúrio
+            intervalIDVenus = setInterval(updateVenusPosition, 24.3); // Retoma o intervalo de Vênus
+            isIntervalRunning = true; // Atualiza a variável para indicar que o intervalo está em execução
+        }
+    }
+});            
   
+
+
+// Descrição do Sol
 document.getElementById("sol").addEventListener("click", function(event) {
     var descricaosol = document.getElementById("descricao-sol");
     var targetElement = event.target;
-    if (!targetElement.classList.contains("Mercurio")) {
+    if (!targetElement.classList.contains("Mercurio") && !targetElement.classList.contains("Venus")) {
         descricaosol.classList.toggle("mostrar");
     }
 });
 
-// Obtenha o elemento de Mercúrio
-const mercurio = document.querySelector('.Mercurio');
 
-// Variáveis para a posição atual de Mercúrio
-let angle = 0;
-const radius = 200; // Raio da órbita
 
 // Função para atualizar a posição de Mercúrio
 function updateMercurioPosition() {
     // Calcula a posição x e y de Mercúrio na órbita circular
-    const x = radius * Math.cos(angle);
-    const y = radius * Math.sin(angle);
+    const x = roMercurio * Math.cos(aMercurio);
+    const y = roMercurio * Math.sin(aMercurio);
 
     // Define a posição de Mercúrio
     mercurio.style.transform = `translate(${x}px, ${y}px)`;
 
     // Incrementa o ângulo para o próximo quadro
-    angle += 0.005; // Ajuste conforme necessário para a velocidade da órbita
+    aMercurio += 0.0088; // Ajuste conforme necessário para a velocidade da órbita
 }
 
-// Atualiza a posição de Mercúrio em intervalos regulares
-setInterval(updateMercurioPosition, 20);
+// Descrição de Mercúrio
+document.getElementById("mercurio").addEventListener("click", function(event) {
+    var descricaosol = document.getElementById("descricao-mercurio");
+    var targetElement = event.target;
+    if (targetElement.classList.contains("Mercurio")) {
+        descricaosol.classList.toggle("mostrar");
+    }
+});
+
+
+
+// Função para atualizar a posição de Vênus
+function updateVenusPosition() {
+    // Calcula a posição x e y de Mercúrio na órbita circular
+    const x = roVenus * Math.cos(aVenus);
+    const y = roVenus * Math.sin(aVenus);
+
+    // Define a posição de Mercúrio
+    venus.style.transform = `translate(${x}px, ${y}px)`;
+
+    // Incrementa o ângulo para o próximo quadro
+    aVenus += 0.0088; // Ajuste conforme necessário para a velocidade da órbita
+}
+
+// Descrição de Vênus
+document.getElementById("venus").addEventListener("click", function(event) {
+    var descricaosol = document.getElementById("descricao-venus");
+    var targetElement = event.target;
+    if (targetElement.classList.contains("Venus")) {
+        descricaosol.classList.toggle("mostrar");
+    }
+});
