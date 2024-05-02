058f0e50dbb3704c85214c8f891bc4913b5f75b6
GuiMV <guilherme.moizinho17@gmail.com>
2024-05-02 11:53:04 -0300

    Adição do Cinturão de Asteróides e de Ceres

--- Space/script-space.js
+++ Space/script-space.js
@@ -5,16 +5,17 @@ const mainElement = document.querySelector("main");
 
 const planets = {
     Sol: { element: document.querySelector('.Sol'), angle: 0, radius: 0, interval: 0, updatePosition: updatePlanetPosition},
-    Mercurio: { element: document.querySelector('.Mercurio'), angle: 0, radius: 65, interval: 8.8, updatePosition: updatePlanetPosition}, //97.32 63.71
-    Venus: { element: document.querySelector('.Venus'), angle: 0, radius: 115, interval: 24.3, updatePosition: updatePlanetPosition}, //137.75 119
-    Terra: { element: document.querySelector('.Terra'), angle: 0, radius: 176, interval: 36.5, updatePosition: updatePlanetPosition}, //178.40
-    Marte: { element: document.querySelector('.Marte'), angle: 0, radius: 230.6, interval: 68.7, updatePosition: updatePlanetPosition}	//227.94 260.94
+    Mercurio: { element: document.querySelector('.Mercurio'), angle: 0, radius: 105, interval: 8.8, updatePosition: updatePlanetPosition},
+    Venus: { element: document.querySelector('.Venus'), angle: 0, radius: 156, interval: 24.3, updatePosition: updatePlanetPosition},
+    Terra: { element: document.querySelector('.Terra'), angle: 0, radius: 197, interval: 36.5, updatePosition: updatePlanetPosition},
+    Marte: { element: document.querySelector('.Marte'), angle: 0, radius: 275, interval: 68.7, updatePosition: updatePlanetPosition},
+    Ceres:  { element: document.querySelector('.Ceres'), angle: 0, radius: 462, interval: 168.2, updatePosition: updatePlanetPosition}
 };
 
 const moon = {
-    Lua: { element: document.querySelector('.Lua'), angle: 0, radius: 13.844, interval: 7, updatePosition: updatePlanetPosition},
-    Fobos: { element: document.querySelector('.Fobos'), angle: 0, radius: 9, interval: 6, updatePosition: updatePlanetPosition},
-    Deimos: { element: document.querySelector('.Deimos'), angle: 0, radius: 23, interval: 12, updatePosition: updatePlanetPosition}
+    Lua: { element: document.querySelector('.Lua'), angle: 0, radius: 10.844, interval: 7, updatePosition: updatePlanetPosition},
+    Fobos: { element: document.querySelector('.Fobos'), angle: 0, radius: 9, interval: 3.5, updatePosition: updatePlanetPosition},
+    Deimos: { element: document.querySelector('.Deimos'), angle: 0, radius: 23, interval: 7, updatePosition: updatePlanetPosition}
 };
 
 // Inicializa os intervalos de atualização para planetas e lua
@@ -79,7 +80,7 @@ function zoom(event, isCustomZoom) {
     const offsetX = (event.clientX - boundingRect.left) / boundingRect.width;
     const offsetY = (event.clientY - boundingRect.top) / boundingRect.height;
     scale += (isCustomZoom ? zoomStep * (event.deltaY < 0 ? 0.5 : -0.5) : (event.deltaY < 0 ? zoomStep : -zoomStep));
-    scale = Math.max(0.7, Math.min(4, scale));
+    scale = Math.max(0.9, Math.min(4, scale));
     mainElement.style.transition = "transform 0.2s ease-in-out";
     mainElement.style.transformOrigin = `${offsetX * 100}% ${offsetY * 100}%`;
     mainElement.style.transform = `scale(${scale})`;
@@ -97,23 +98,55 @@ function showCelestialBodyDescription(celestialBodyName, descriptionElementId) {
         const clickedElement = event.target;
         const descriptionElement = document.getElementById(descriptionElementId);
 
-        // Verifica se o corpo celeste clicado corresponde ao celestialBodyName
         if (clickedElement.classList.contains(celestialBodyName)) {
-            // Fecha todas as outras descrições
             descriptionElements.forEach(element => {
                 if (element.id !== descriptionElementId) {
                     element.classList.remove("mostrar");
                 }
             });
-
-            // Exibe ou oculta a descrição do planeta ou lua clicado
             descriptionElement.classList.toggle("mostrar");
         } else {
-            // Fecha a descrição se o clique não estiver no corpo celeste
             descriptionElement.classList.remove("mostrar");
         }
     });
 }
+
+// Função para gerar pontos aleatórios dentro do cinturão de asteroides
+function AnelDeAsteroides() {
+    var numAsteroides = 300;
+    var raioExterno = 450;
+    var raioInterno = 400;
+    var centroX = 0;
+    var centroY = 0;
+    var container = document.getElementById('cinturao');
+
+    var asteroideCentralExistente = document.querySelector('.asteroide-central');
+    if (asteroideCentralExistente) {
+        container.removeChild(asteroideCentralExistente);
+    }
+
+    for (var i = 0; i < numAsteroides; i++) {
+        var angulo = (i / numAsteroides) * Math.PI * 2;
+        var x = centroX + raioExterno * Math.cos(angulo);
+        var y = centroY + raioExterno * Math.sin(angulo);
+
+        // Ajuste do raio interno
+        var proporcaoRaioInterno = Math.random(); // Proporção aleatória entre 0 e 1
+        var raio = raioInterno + (raioExterno - raioInterno) * proporcaoRaioInterno;
+
+        x = centroX + raio * Math.cos(angulo);
+        y = centroY + raio * Math.sin(angulo);
+
+        var asteroide = document.createElement('div');
+        asteroide.className = 'ponto-asteroide';
+        asteroide.style.left = x + 'px';
+        asteroide.style.top = y + 'px';
+        container.appendChild(asteroide);
+    }
+}
+
+// Chamar a função para gerar os pontos de asteroides ao carregar a página
+window.onload = AnelDeAsteroides;
 
 // Exibir descrições dos planetas e lua
 showCelestialBodyDescription("Sol", "descricao-sol");
@@ -124,6 +157,7 @@ showCelestialBodyDescription("Lua", "descricao-lua");
 showCelestialBodyDescription("Marte", "descricao-marte");
 showCelestialBodyDescription("Fobos", "descricao-fobos");
 showCelestialBodyDescription("Deimos", "descricao-deimos");
+showCelestialBodyDescription("Ceres", "descricao-ceres");
 
 document.addEventListener('keydown', event => {
     if (event.code === 'Space') {
